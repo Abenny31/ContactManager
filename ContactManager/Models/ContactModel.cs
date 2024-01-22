b82aa7b76c629b8b2504f14b826961dc5d82b089
ABenny31 <ante_benic@hotmail.com>
2024-01-22 13:54:48 +0100

    added contact list

---
+++ ContactManager/Models/ContactModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Menu;
+
+namespace ContactManager.Models
+{
+    internal class ContactModel
+    {
+        internal int Id { get; set; }
+        internal string Name { get; set; }
+        internal string Surname { get; set; }
+        internal DateTime DateOfBirth { get; set; }
+        internal char Sex { get; set; }
+        List<string> PhoneNumber { get; set; }
+
+
+    }
+}
